075514965824ae62f340eca2d9d6f16bd529d068
Gleizits <98473712+Gleizits@users.noreply.github.com>
2022-04-08 11:01:55 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+#include "conio.h"
+using namespace std;
+int main(){
+    float a;
+    int b;
+    char c;
+    cout<<"Enter your height: "<<endl; cin>>a;
+    cout<<"Enter your age: "<<endl; cin>>b;
+    cout<<"Enter your sex: "<<endl; cin>>c;
+    cout<<"Your height is: "<<a<<endl;
+    cout<<"Your age is: "<<b<<endl;
+    cout<<"Your sex is: "<<c<<endl;
+    getch();
+    return 0;
+}
